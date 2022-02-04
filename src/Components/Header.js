7c9ff226bc6f2c4b04d1bbc1e5f3cc5207be7af0
Baboo523 <babooheerani999@gmail.com>
2022-02-04 09:56:15 +0500

    Update all

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,54 +1,102 @@
 import React, { Component } from 'react';
-import LOGOIMAGE from '../images/logo.png' 
-
-class Logo extends React.Component{
-    render(){
-        return(
+import LOGOIMAGE from '../images/Saylani.PNG';
+import '../App.css';
+class Logo extends React.Component {
+    render() {
+        return (
             <div>
-                <img src={LOGOIMAGE} alt="" width="100" />
+                <img className='setting' src={LOGOIMAGE} alt="" width="150px" height="50px" />
             </div>
         )
-    }   
+    }
 }
-
-class Header extends React.Component{
-    render(){
-        return(
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+class Header extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light float">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">
                         <Logo />
                     </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon" />
+                        <span className="navbar-toggler-icon" />
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a className="dropdown-item" href="#">Action</a></li>
-                            <li><a className="dropdown-item" href="#">Another action</a></li>
-                            <li><hr className="dropdown-divider" /></li>
-                            <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    About
+                                </a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a className="dropdown-item" href="#">Introducation</a></li>
+                                    <li><a className="dropdown-item" href="#">Chairman Message</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Saylani Annual Report</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                   Saylani Services
+                                </a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a className="dropdown-item" href="#">Education</a></li>
+                                    <li><a className="dropdown-item" href="#">Medical</a></li>
+                                    <li><a className="dropdown-item" href="#">Welfare</a></li>
+                                    <li><a className="dropdown-item" href="#">Hajj Application Form</a></li>
+                                    <li><a className="dropdown-item" href="#">Saylani Dastrkhawan</a></li>
+                                    <li><a className="dropdown-item" href="#">Saylani RO Plant</a></li>
+                                    <li><a className="dropdown-item" href="#">Online Sadqah</a></li>
+                                    <li><a className="dropdown-item" href="#">Online Taweezat/Istekhara </a></li>
+                                    <li><a className="dropdown-item" href="#">Saylani Job Bank</a></li>
+                                    <li><a className="dropdown-item" href="#">Online Quran Academy</a></li>
+                                    <li><a className="dropdown-item" href="#">Books </a></li>
+                                    <li><a className="dropdown-item" href="#">Saylani Online Masajid</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Saylani Ehsass
+                                </a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a className="dropdown-item" href="#">Saylani Langarkhana</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Future Project
+                                </a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a className="dropdown-item" href="#">Saylani University</a></li>
+                                    <li><a className="dropdown-item" href="#">Saylani City</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Media</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">News</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Donation
+                                </a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a className="dropdown-item" href="#">Bank Details</a></li>
+                                    <li><a className="dropdown-item" href="#">Sadqa</a></li>
+                                    <li><a className="dropdown-item" href="#">Aqiqa</a></li>
+                                    <li><a className="dropdown-item" href="#">Other Donation</a></li>
+                                    <li><a className="dropdown-item" href="#">Corona Effets</a></li>
+                                    <li><a className="dropdown-item" href="#">Contact Us</a></li>
+                                </ul>
+                            </li>
                         </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled">Disabled</a>
-                        </li>
-                        </ul>
-                        <form className="d-flex">
+                        {/* <form className="d-flex">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
+                        </form> */}
                     </div>
                 </div>
             </nav>
---
+++ src/Components/body.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+// import LOGOIMAGE from '../images/Saylani.png' 
+import Slider1 from '../images/slider1.PNG';
+import Slider2 from '../images/slider 2.PNG';
+import Slider3 from '../images/slider 3.PNG';
+import Slider4 from '../images/slider 4.PNG';
+import Slider5 from '../images/slider 5.PNG';
+import img from '../images/say.png';
+import Meal from '../images/removebg1.png';
+import People from '../images/people.PNG';
+import Doctor from '../images/doc.PNG';
+import cap from '../images/Capture.PNG';
+import '../App.css';
+
+
+// import icon1 from '../images/1.png';
+// import icon2 from '../images/2.png';
+// import icon3 from '../images/3.png';
+// import icon4 from '../images/4.png';
+
+
+
+class Body extends React.Component {
+    render() {
+        return (
+            <div>
+                <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src={Slider1} className="d-block w-100" alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src={Slider2} className="d-block w-100" alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src={Slider3} className="d-block w-100" alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src={Slider4} className="d-block w-100" alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src={Slider5} className="d-block w-100" alt="..." />
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true" />
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true" />
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
+                <br />
+                <marquee style={{ backgroundColor: "blue", color: "white" }} behavior="scroll" direction="left" scrollamount={20}>(News-1 Dated:15-05-2019)  Saylani Welfare Announced "Donation Application" . More Details    (News-2 Dated:07-05-2019)  Alhamdulillah Saylani Organized 200+ Locations For Iftar And Sehri In This Holy Month . More Details    (News-3 Dated:08-03-2019)  Saylani Welfare Distributing Ration & Clothes to affected people of Balochistan. More Details    (News-4 Dated:25-02-2019)  PIAIC Entrance Exam Merit Ranking List Announced. More Details   (News-5 Dated:23-02-2019)  President of Pakistan (Dr. Arif Alvi) Visited Saylani Welfare International Trust HeadOffice Karachi. More Details    (News-6 Dated:19-02-2019)  Dr Jamil Ahmed (Additional Inspector General of Police) & Atiqmir Visit Saylani Welfare International Trust, Saylani Mass I.T. Training. More Details</marquee>
+
+                <br /> <br /> <br />
+
+                <h2 style={{ textAlign: "center" }}>INTRODUCATION</h2>
+                <img src={img} alt="cur" className='Image' />
+                <br /> <br />
+                <div className='center' >
+                    <p>
+                        Established in May 1999 by famous spiritual and religious scholar Maulana Bashir Ahmed Farooqui, Saylani Welfare International Trust was built on the fundamentals of breaking the cycle of poverty, alleviating the financial troubles of the poor, giving people a chance to live a       dignified life and spreading happiness.
+                    </p>
+                    <p>
+                        Established in May 1999 by famous spiritual and religious scholar Maulana Bashir Ahmed Farooqui, Saylani Welfare International Trust was built on the fundamentals of breaking the cycle of poverty, alleviating the financial troubles of the poor, giving people a chance to live a       dignified life and spreading happiness.
+                    </p>
+                    <p>
+                        Today, we are proud to say that our physical presence extends throughout Pakistan with a vast network of 125 branches operating in major cities including Karachi, Lahore, Islamabad, Rawalpindi, Hyderabad and Faisalabad. Apart from Pakistan, we have overseas offices in the UK, USA and UAE as well. Our worldwide branches are operating under the guidance of a team of over 3,500 working professionals who help almost 150,000 people on a daily basis.
+                    </p>
+                    <p>
+                        We strongly believe that a little help goes a long way and our work would not be possible without the generous support of our valuable donors. Our local and international donors have graciously lent us a hand by supporting our causes and enabling us to serve the needs of people in need.
+                    </p>
+                </div>
+                <div>
+                    <div className="main">
+                        <span className="meal">
+                            <center><img src={Meal} alt="..." height="50px" width="50px" /></center>
+                            <center><h1><b>150,000</b></h1></center>
+                            <center><p><b>DAILY DASTRAKHAN(MONTHLY) </b></p></center>
+                        </span>
+                        <span className="fam">
+                            <center><img src={People} alt="..." height="50px" width="50px" /></center>
+                            <center><h1><b>8,500</b></h1></center>
+                            <center><p><b>FAMILY ADOPTION(MONTHLY) </b></p></center>
+                        </span>
+                        <span className="doc">
+                            <center><img src={Doctor} alt="..." height="50px" width="50px" /></center>
+                            <center><h1><b>53,749</b></h1></center>
+                            <center><p><b>MEDICAL(MONTHLY) </b></p></center>
+                        </span>
+                        <span className="edu">
+                            <center><img src={cap} alt="..." height="50px" width="50px" /></center>
+                            <center><h1><b>180,000</b></h1></center>
+                            <center><p><b>Education(MONTHLY) </b></p></center>
+                        </span>
+                    </div>
+                    <br /> <br />
+                    <h1 style={{ textAlign: 'center' }} >WHAT WE ARE DOING</h1>
+                    <img src={img} alt="cur" className='Image' />
+                    <br /> <br />
+                </div>
+            </div>
+        )
+    }
+}
+export default Body;
